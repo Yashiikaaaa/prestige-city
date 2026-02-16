a67ad8e59a0251fd268f41446525066c942390f8
yashiikaaaa <yashika@truestate.in>
2026-02-16 15:49:31 +0530

    xyz

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Features = () => {
   // Array containing the features data
  const featuresData = [
-    { id: 1, title: 'Project Size', value: '6+ acres' },
+    { id: 1, title: 'Project Size', value: '10+ acres' },
     { id: 2, title: 'Possession', value: '2029 ' },
     { id: 3, title: 'Starting Price', value: '₹ 1.38 Cr*' },
     { id: 4, title: 'Configurations', value: '2, 3 & 4 BHK' },
